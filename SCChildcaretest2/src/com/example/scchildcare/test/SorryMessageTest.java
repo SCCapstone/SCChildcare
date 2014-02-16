4431aac1a7e8838dd78885335dad78fb9a01c4b6
Nick Bowe <Confederate803@me.com>
2014-02-16 18:25:56 -0500

    adding Testing

---
+++ SCChildcaretest2/src/com/example/scchildcare/test/SorryMessageTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.example.scchildcare.test;
+import com.example.scchildcare.*;
+
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * @author Confederate803
+ *
+ */
+public class SorryMessageTest extends ActivityInstrumentationTestCase2<SorryMessageActivity> {
+
+	/**
+	 * @param name
+	 */
+	public SorryMessageTest(String name) {
+		super(SorryMessageActivity.class);
+	}
+
+	/* (non-Javadoc)
+	 * @see android.test.ActivityInstrumentationTestCase2#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
