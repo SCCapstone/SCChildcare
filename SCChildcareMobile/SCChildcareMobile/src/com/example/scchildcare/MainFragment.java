3e94a3263a5520fddb61bf671c031301827ca966
LeCanadien <ogunjij@email.sc.edu>
2014-02-17 04:09:32 -0500

    debugging

---
+++ SCChildcareMobile/SCChildcareMobile/src/com/example/scchildcare/MainFragment.java
@@ -0,0 +1,107 @@
+package com.example.scchildcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+//import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+//import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
+//import com.facebook.widget.LoginButton;
+
+public class MainFragment extends Fragment {
+	private UiLifecycleHelper uiHelper;
+	private static final String TAG = "MainFragment";
+	public static String message = null;
+	EditText editText;
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		@Override
+		public void call(Session session, SessionState state,
+				Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
+	};
+
+	public String locate2(View view) {
+		String message = editText.getText().toString();
+		return message;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.activity_main, container, false);
+		editText = (EditText) view.findViewById(R.id.edit_message);
+		 LoginButton authButton = (LoginButton)
+		 view.findViewById(R.id.authButton);
+		 authButton.setFragment(this);
+		System.out.println("Hey I'm in onCreateView");
+		return view;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+		uiHelper.onCreate(savedInstanceState);
+		System.out.println("I've reached fragment onCreate");
+
+	}
+
+	private void onSessionStateChange(Session session, SessionState state,
+			Exception exception) {
+		if (state.isOpened()) {
+			Log.i(TAG, "Logged in...");
+		} else if (state.isClosed()) {
+			Log.i(TAG, "Logged out...");
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		// For scenarios where the main activity is launched and user
+		// session is not null, the session state change notification
+		// may not be triggered. Trigger it if it's open/closed.
+		Session session = Session.getActiveSession();
+		if (session != null && (session.isOpened() || session.isClosed())) {
+			onSessionStateChange(session, session.getState(), null);
+		}
+
+		uiHelper.onResume();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		uiHelper.onSaveInstanceState(outState);
+	}
+
+}
--- SCChildcareMobileTest/src/com/example/scchildcare/test/MainActivityTest.java
+++ SCChildcareMobileTest/src/com/example/scchildcare/test/MainActivityTest.java
@@ -4,11 +4,14 @@ import com.example.scchildcare.MainActivity;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    private MainActivity mainActivity;
-    private EditText mEditText;
+   private MainActivity mainActivity;
+   private EditText mEditText;
+   private ImageButton b1;
+   private ImageButton b2;
 	
 	
 	public MainActivityTest(String name) {
@@ -19,8 +22,27 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 		super.setUp();
 		mainActivity = getActivity();
 		mEditText = (EditText) mainActivity.findViewById(com.example.scchildcare.R.id.edit_message);
+		b1 = (ImageButton) mainActivity.findViewById(com.example.scchildcare.R.id.button1);
+		b2 = (ImageButton) mainActivity.findViewById(com.example.scchildcare.R.id.button2);
 	}
-
+	
+	 public void testPreconditions() {
+		    assertNotNull("Main Activity is null", mainActivity);
+	        assertNotNull("editText is null", mEditText);
+	        assertNotNull("button 1 is null", b1);
+	        assertNotNull("button 2 is null", b2);
+	    }
+	
+	public void testGPSlocationButton(){
+	//make sure that clicking the button issues intent	
+		
+	}
+	
+	public void testSendMessageButton(){
+		//make sure that clicking the button issues intent	
+			
+			
+		}
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
