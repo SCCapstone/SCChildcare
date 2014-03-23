7bf668ee7e3ee82bd6d5d57bf1b7846767e55e20
Nick Bowe <Confederate803@me.com>
2014-03-23 18:25:23 -0400

    update testing

--- SCChildcaretest2/src/com/example/scchildcare/test/MainActivityTest.java
+++ SCChildcaretest2/src/com/example/scchildcare/test/MainActivityTest.java
@@ -1,6 +1,7 @@
 package com.example.scchildcare.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.scchildcare.*;
@@ -9,7 +10,9 @@ import junit.framework.TestCase;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	private MainActivity mockMainActivity;
-	private EditText mockEditText; 
+	private EditText mockaddressText; 
+	private EditText mockEditText;
+	private Button mockButton1;
 	public MainActivityTest()
 	{
 		super(MainActivity.class);
@@ -17,10 +20,14 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		setActivityInitialTouchMode(true);
 		mockMainActivity = getActivity();
+		mockButton1 = (Button) mockMainActivity.findViewById(R.id.
 	}
 	public void testPreconditios() {
 		assertNotNull("mockMainActivity is null", mockMainActivity);
-		assertNotNull("mockEditText is null",mockEditText);
+		assertNotNull("mockEditText is null", mockEditText);
+		assertNotNull("mockaddressText is null", mockaddressText);
+		assertNotNull("mockButton1", mockButton1);
 	}
 }
