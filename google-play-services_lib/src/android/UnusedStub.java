8614cf554b438d9379941aa492110c96e52f865d
heavenscloud001 <gaddybb@email.sc.edu>
2014-04-06 14:27:36 -0400

    Updating Ben

--- google-play-services_lib/src/android/UnusedStub.java
+++ google-play-services_lib/src/android/UnusedStub.java
@@ -16,6 +16,6 @@
 package android;
 
 // Stub java file to make inclusion into some IDE's work.
-public final class UnusedStub {
-    private UnusedStub() { }
-}
+//public final class UnusedStub {
+//    private UnusedStub() { }
+//}
